64c7d3237076fadb0ef5c82a32709be97fd08673
juyony <wnsdyd1991@gamil.com>
2023-09-21 15:42:32 +0900

    주가 크롤링

--- api.py
+++ api.py
@@ -43,7 +43,9 @@ parse_day = data_html.select_one(
 biz_day = re.findall('[0-9]+', parse_day)
 biz_day = ''.join(biz_day)
 
+
 #ticker
+
 ## 코스피 (stk)
 gen_otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
 gen_otp_stk = {
@@ -57,9 +59,8 @@ gen_otp_stk = {
 }
 ### OTP 받기 전 나의 행적을 알려주기
 headers = {'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'}
-
+# 이 부분(otp)을 url에 제출하면 데이터를 받을 수 있음.
 otp_stk = rq.post(gen_otp_url, gen_otp_stk, headers=headers).text
-otp_stk # 이 부분(otp)을 url에 제출하면 데이터를 받을 수 있음.
 
 down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
 down_sector_stk = rq.post(down_url, {'code': otp_stk}, headers=headers)
@@ -67,7 +68,6 @@ down_sector_stk.content # 받은 csv파일이 html형태로 나타남.
 
 ### 클랜징 처리 & 인코딩
 sector_stk = pd.read_csv(BytesIO(down_sector_stk.content), encoding = 'EUC-KR')
-
 
 
 ## 코스닥 (ksq)
@@ -85,8 +85,6 @@ gen_otp_ksq = {
 headers = {'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'}
 
 otp_ksq = rq.post(gen_otp_url, gen_otp_ksq, headers=headers).text
-otp_ksq 
-
 down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
 down_sector_ksq = rq.post(down_url, {'code': otp_ksq}, headers=headers)
 down_sector_ksq.content 
@@ -96,14 +94,10 @@ sector_ksq = pd.read_csv(BytesIO(down_sector_ksq.content), encoding = 'EUC-KR')
 
 # concat stk, ksq
 krx_sector = pd.concat([sector_stk, sector_ksq]).reset_index(drop = True)
-krx_sector
 
 # 종목명 null 데이터 클랜징 (strip) & 기준일 추가.
 krx_sector['종목명'] = krx_sector['종목명'].str.strip()
 krx_sector['기준일'] = biz_day
-
-
-
 
 
 #sector
@@ -125,8 +119,66 @@ for i in tqdm(sector_code):
     time.sleep(1)
 
 kor_sector = pd.concat(data_sector, axis = 0)
-kor_sector
 kor_sector = kor_sector[['IDX_CD', 'CMP_CD', 'CMP_KOR', 'SEC_NM_KOR']]
 kor_sector['기준일'] = biz_day
 kor_sector['기준일'] = pd.to_datetime(kor_sector['기준일'])
+
+# 삭제된 종목, 추가된 종목이 있을 수 있어 replace
+kor_ticker.to_sql(name='kor_ticker', con=engine, index=True, if_exists='replace')
+kor_sector.to_sql(name='kor_sector', con=engine, index=True, if_exists='replace')
+
+
+# 주가 데이터
+# 티커 리스트 불러오기
+ticker_list = pd.read_sql("""
+                    select * from kor_ticker
+                    where 기준일 = (select max(기준일) from kor_ticker) and 종목구분 = '보통주';
+                    """, con=engine)                          
+
+# 오류 발생시 저장할 리스트 생성
+error_list = []
+
+# 전종목 주가 다운로드 및 저장
+for i in tqdm(range(0, len(ticker_list))):
+    
+    # 티커 선택
+    ticker = ticker_list['종목코드'][i]
+    
+    # 시작일과 종료일
+    #fr = (date.today() + relativedelta(years=-5)).strftime('%Y%m%d')
+    fr = (date.today() + relativedelta(days = -1)).strftime("%Y%m%d")
+    to = (date.today()).strftime('%Y%m%d')
+    
+    
+    # 오류 발생 시 이를 무시하고 다음 루프로 진행
+    try:
+    
+        # url 생성
+        url = f'https://api.finance.naver.com/siseJson.naver?symbol={ticker}&requestType=1&startTime={fr}&endTime={to}&timeframe=day'
+        
+        # 데이터 다운로드
+        data = rq.get(url).content
+        data_price = pd.read_csv(BytesIO(data))
+        
+        # 데이터 클랜징
+        price = data_price.iloc[:, 0:6]
+        price.columns = ['날짜', '시가', '고가', '저가', '종가', '거래량']
+        price = price.dropna()
+        price['날짜'] = price['날짜'].str.extract('(\d+)')
+        price['날짜'] = pd.to_datetime(price['날짜'])
+        price['종목코드'] = ticker
+        price['종목명'] = ticker_list[ticker_list['종목코드'] == ticker]['종목명'].values[0]
+
+        # 주가 데이터를 DB에 저장
+        price.to_sql(name="stock_price", con=engine, index=True, if_exists='append')
+        
+    except:
+        
+        # 오류 발생 시 error_list에 티커 저장하고 넘어가기
+        print(ticker)
+        error_list.append(ticker)
+    
+    # 타임슬립 적용
+    time.sleep(1)
+
 
